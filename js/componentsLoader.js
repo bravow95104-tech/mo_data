a02c385ca813bd511e00384d076b7ce1db28cbb4
zengfes <qazwsx83251gh@gmail.com>
2025-10-01 00:43:18 +0800

    fix

--- js/componentsLoader.js
+++ js/componentsLoader.js
@@ -14,6 +14,6 @@ function loadComponent(selector, url) {
 
 // 自動載入 nav（如有需要可擴充更多元件）
 document.addEventListener('DOMContentLoaded', function() {
-  loadComponent('#nav-placeholder', '../../components/nav/nav.html');
-  loadComponent('#footer-container', '../../components/footer/footer.html');
+  loadComponent('#nav-placeholder', '../components/nav/nav.html');
+  loadComponent('#footer-container', '../components/footer/footer.html');
 });
