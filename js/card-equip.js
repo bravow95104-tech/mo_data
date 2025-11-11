d541f8de6f5d677a02ebd490c510626e21136c21
bravow95104-tech <bravow95104@gmail.com>
2025-11-11 19:28:41 +0800

    Add files via upload

--- js/card-equip.js
+++ js/card-equip.js
@@ -139,9 +139,9 @@ function showDetailModal(item) {
   img.style.height = 'auto';
   img.style.objectFit = 'contain';
 
-  // 編碼檔名但保留 _ 和 .
+  // 編碼檔名但保留 _ 和. 。
 function encodeFileName(name) {
-  return name.replace(/[^\w\u4e00-\u9fa5().]/g, '');
+  return name;  //name.replace(/[^\w\u4e00-\u9fa5().-]/g, '');
 }
 
   // 候選圖片路徑
