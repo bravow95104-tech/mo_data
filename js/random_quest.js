889766a2e785a4f46ff08d216c801c14340b5beb
bravow95104-tech <bravow95104@gmail.com>
2026-06-26 22:16:47 +0800

    Update random_quest.js

--- js/random_quest.js
+++ js/random_quest.js
@@ -1,5 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-    import { SUPABASE_URL, SUPABASE_KEY } from './js/supabase-config.js'
+    import { SUPABASE_URL, SUPABASE_KEY } from './supabase-config.js'
 
     const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
     
