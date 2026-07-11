97714fea42a438c6a6ac02d7175a1d1fde61fa23
bravow95104-tech <bravow95104@gmail.com>
2026-07-11 13:05:56 +0800

    網頁與圖片快速更新

--- js/calculation.js
+++ js/calculation.js
@@ -194,7 +194,7 @@ document.addEventListener("DOMContentLoaded", () => {
           if (radio.value === "2x") multiplier = 2;
           else if (radio.value === "2.5x") multiplier = 2.5;
         }
-        equipTotal += val * multiplier;
+         equipTotal += Math.floor(val * multiplier);
       });
       const transformedPoint = isTransformed ? 800 : 0;
       const transformedRemainingPoints = isTransformed ? 2520 : 1720;
